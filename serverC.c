a6c079cc2b59c77e584441ad452f1d77e850c62a
Hui Huang <huihuang_0506@outlook.com>
2017-02-01 19:22:50 -0800

    Update serverC.c

--- serverC.c
+++ serverC.c
@@ -1,6 +1,5 @@
+#include "aws.h"
 #include "servers.h"
-#include "hui_fun.h"
-
 
 void error(char *msg)
 {
@@ -13,8 +12,9 @@ int main()
 	int sock, length, fromlen, n;
 	struct sockaddr_in server;
 	struct sockaddr_in from;
-	char buf[1024];
+	char buf[3000];
 
+	//establish UDP socket in serverC
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sock < 0)
 	{
@@ -26,22 +26,36 @@ int main()
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = 23205;
+
+	//bind socket with IP address and port number
 	if (bind (sock,(struct sockaddr *)&server, length) < 0)
 	{
 		error("binding");
 	}
 	fromlen = sizeof(struct sockaddr_in);
+	printf("%s\n", "The Server C is up and running using UDP on port <23205>");
 
 	while (1)
 	{
-		n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&from, &fromlen);
+
+
+		memset(buf, '\0', sizeof(buf));
+		//receive data from aws in the form of char array
+		n = recvfrom(sock, buf, 3000, 0, (struct sockaddr *)&from, &fromlen);
+
+		if (n < 0)
+		{
+			error("recvfrom");
+		}
 
 		int count = 2;
 		int index = 0;
 		int num_fun = atoi(buf);
-		int result[300];
+		int result[400];
 	    memset(&result, 0, sizeof(result));
 
+
+	    //transform the receving data from char to integer
 		while (count < strlen(buf) - 1)
 		{
 
@@ -54,56 +68,58 @@ int main()
 			count++;
 
 		}
-		for (int i = 0; i < index; i++){
-			printf("%d    ", result[i]);
-		}
+
+		printf("%s%d%s\n", "The Server A has received ",index," numbers");
 		
-		if (n < 0)
-		{
-			error("recvfrom");
-		}
 
-		int Res;
+		long long int Res;
+		char fun[10];
 
+		
+		//realize the four functions according to the function number
 		switch (num_fun)
 		{
 			case 1 :
 			Res = Sum(result, index);
+			strcpy(fun, "SUM");
 			break;
 
 			case 2 :
 			Res = Max(result, index);
+			strcpy(fun, "MAX");
 			break;
 
 			case 3 :
 			Res = Min(result, index);
+			strcpy(fun, "MIN");
 			break;
 
 			case 4 :
 			Res = Sos(result, index);
+			strcpy(fun, "SOS");
 			break;
 
 			default:
-			printf("%s\n", "error");
+			error("ERROR function number");
 
 		}
-		printf("\n%d\n", Res);
 
+		printf("%s%s%s%lld\n", "The Server C has successfully finished the reduction ",fun,": ",Res);
+		//transform the final result to integer form
 		memset(&result, 0, sizeof(result));
 		index = 0;
-		write(1, "Received a datagram : ", 21);
-		write(1, buf, n);
-		char sendmessage[300];
+		char sendmessage[400];
 		memset(&sendmessage, 0, sizeof(sendmessage));
 		_itoa(Res, sendmessage, 10);
-		printf("%s\n", sendmessage);
 		char *sendmes = sendmessage;
 
-		
+		//send the data to aws
 		n = sendto(sock, sendmes, strlen(sendmessage) + 1, 0, (struct sockaddr *)&from, fromlen);
 		if (n < 0)
 		{
 			error("sendto");
 		}
+
+		printf("%s\n\n", "The Server C has successfully finished sending the reduction value to AWS");
 	}
 }
